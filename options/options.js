a3b81311b39622604241758f86cba8c105377e10
Crowdbotics <team@crowdbotics.com>
2024-03-08 22:40:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ecycler-46899",
+  url: "https://ecycler-46899.botics.co",
+  api: "https://ecycler-46899.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
